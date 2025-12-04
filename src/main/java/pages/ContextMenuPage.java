f2ae471b0b543cb51befa412435d51c2f295e7bf
emre-banking <emrealagoz@gmail.com>
2025-12-05 01:22:37 +0300

    Update ContextMenuPage.java

--- src/main/java/pages/ContextMenuPage.java
+++ src/main/java/pages/ContextMenuPage.java
@@ -1,6 +1,4 @@
-// This class represents the Context Menu Page of the application.
-// It provides methods to perform actions related to the context menu.
-// The constructor takes a WebDriver instance to interact with the browser.
+// This class provides methods to perform actions related to the context menu.
 
 package pages;
 
@@ -9,29 +7,32 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
-public class ContextMenuPage {
+public class ContextMenuPage extends BasePage {
 
-    private WebDriver driver;
-    private By frame = By.id("hot-spot");
+    private final Actions actions;
+    private final By hotSpotArea = By.id("hot-spot");
 
     public ContextMenuPage(WebDriver driver){
-        this.driver=driver;
+        super(driver);
+        this.actions = new Actions(driver);
     }
 
     // Performs a context click on the specified target element.
-    public void contextClick(){
-        Actions actions = new Actions(driver);
-        WebElement target = driver.findElement(frame);
-        actions.contextClick(target).perform();
+    public void contextClick() {
+        actions.contextClick(getHotSpot()).perform();
     }
 
     // Retrieves the text displayed in the alert dialog.
-    public String alert_getText(){
-        return driver.switchTo().alert().getText();
+    public String getAlertText() {
+        return getAlert().getText();
     }
 
     // Accepts the alert dialog by clicking the "OK" button.
-    public void alert_clickToAccept(){
-        driver.switchTo().alert().accept();
+    public void acceptAlert() {
+        getAlert().accept();
+    }
+
+    private WebElement getHotSpot() {
+        return driver.findElement(hotSpotArea);
     }
 }

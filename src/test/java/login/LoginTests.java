7d9447aefcf338e4d232f4ac3d87995f3776b9b8
emre-banking <emrealagoz@gmail.com>
2025-12-05 01:34:17 +0300

    Minor update

--- src/test/java/login/LoginTests.java
+++ src/test/java/login/LoginTests.java
@@ -6,8 +6,6 @@ import base.BaseTests;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 import pages.SecureAreaPage;
-
-import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class LoginTests extends BaseTests {

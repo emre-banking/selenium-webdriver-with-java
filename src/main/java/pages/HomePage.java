c33ad159346876930fc5cf23de179d4eefb969dd
emre-banking <emrealagoz@gmail.com>
2025-12-05 00:44:12 +0300

    Add BasePage.Java

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,21 +1,12 @@
-// This class provides methods to navigate to the different pages and return the respective Page objects.
-
 package pages;
 
 import constants.HomeLinks;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration;
 
-public class HomePage {
-    private static final Duration TIMEOUT = Duration.ofSeconds(5);
-    private final WebDriver driver;
+public class HomePage extends BasePage {
 
     public HomePage(WebDriver driver){
-        this.driver=driver;
+        super(driver);
     }
 
     public ContextMenuPage clickContextMenu(){
@@ -52,21 +43,5 @@ public class HomePage {
 
     public WYSIWYGEditorPage clickWYSIWYGEditor(){
         return navigateTo(HomeLinks.WYSIWYG_EDITOR, WYSIWYGEditorPage.class);
-    }
-
-    private void clickLink(String linkText){
-        WebDriverWait wait = new WebDriverWait(driver, TIMEOUT);
-        By locator = By.xpath("//a[normalize-space()='" + linkText + "']");
-        WebElement link = wait.until(ExpectedConditions.elementToBeClickable(locator));
-        link.click();
-    }
-
-    private <T> T navigateTo(HomeLinks link, Class<T> pageClass){
-        clickLink(link.toString());
-        try {
-            return pageClass.getDeclaredConstructor(WebDriver.class).newInstance(driver);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to create page: " + pageClass.getSimpleName(), e);
-        }
     }
 }

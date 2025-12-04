90a87f0db64d430ec718cffff22d6d74ebd6ac69
emre-banking <emrealagoz@gmail.com>
2025-12-05 02:03:20 +0300

    Refine DropdownPage.java

--- src/main/java/pages/DropdownPage.java
+++ src/main/java/pages/DropdownPage.java
@@ -1,6 +1,4 @@
-// This class represents the Dropdown Page of the application.
-// It provides methods to interact with the dropdown list.
-// The constructor takes a WebDriver instance to interact with the browser.
+// This class provides methods to interact with the dropdown list.
 
 package pages;
 
@@ -8,16 +6,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class DropdownPage {
-    private WebDriver driver;
-    private By dropdownList = By.id("dropdown");
+public class DropdownPage extends BasePage {
+    private final By dropdownList = By.id("dropdown");
 
     public DropdownPage(WebDriver driver){
-        this.driver=driver;
+        super(driver);
     }
 
     // Selects the specified option from the dropdown list.
@@ -27,15 +23,12 @@ public class DropdownPage {
 
     // Returns a list of selected options from the dropdown list.
     public List<String> getSelectedOptions(){
-        List<WebElement> selectedElements =
-                findDropdownElement().getAllSelectedOptions();
-        return selectedElements.stream().map(e->e.getText()).collect(Collectors.toList());
+        List<WebElement> selectedElements = findDropdownElement().getAllSelectedOptions();
+        return selectedElements.stream().map(WebElement::getText).collect(Collectors.toList());
     }
 
     // Finds and returns the dropdown element as a Select object.
     private Select findDropdownElement(){
         return new Select(driver.findElement(dropdownList));
     }
-
-
 }

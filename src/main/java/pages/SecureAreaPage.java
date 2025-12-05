f140db5dccd51b88d24c697eb9ef2d1986847dc3
emre-banking <emrealagoz@gmail.com>
2025-12-05 23:32:17 +0300

    refine SecureAreaPage.java

--- src/main/java/pages/SecureAreaPage.java
+++ src/main/java/pages/SecureAreaPage.java
@@ -1,22 +1,23 @@
-// This class represents the Secure Area Page of the application.
-// It provides a constructor to initialize the WebDriver instance and contains a method to retrieve the alert text.
+// This class provides a constructor to initialize the WebDriver instance and contains a method to retrieve the alert text.
 
 package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
-public class SecureAreaPage {
+public class SecureAreaPage extends BasePage {
 
-    private WebDriver driver;
-    private By statusAlert = By.id("flash");
+    private final By statusAlert = By.id("flash");
 
     public SecureAreaPage(WebDriver driver){
-        this.driver=driver;
+        super(driver);
     }
 
     // Retrieves the text of the status alert on the Secure Area Page
-    public String getAlertText(){
-        return driver.findElement(statusAlert).getText();
+    public String getAlertText() {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(statusAlert))
+                .getText()
+                .trim();
     }
 }

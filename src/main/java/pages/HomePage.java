65a25f08667636e4384c11eed703207c00628077
emre-banking <emrealagoz@gmail.com>
2025-12-06 23:00:18 +0300

    Add Allure.step for navigation functions

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -16,35 +16,51 @@ public class HomePage extends BasePage {
         );
     }
 
-    public DropdownPage navigateToDropdown(){
-        return navigateTo(HomeLinks.DROPDOWN, DropdownPage.class);
+    public DropdownPage navigateToDropdown() {
+        return Allure.step("Navigate to Dropdown page",
+                () -> navigateTo(HomeLinks.DROPDOWN, DropdownPage.class)
+        );
     }
 
-    public DynamicLoadingPage navigateToDynamicLoading(){
-        return navigateTo(HomeLinks.DYNAMIC_LOADING, DynamicLoadingPage.class);
+    public DynamicLoadingPage navigateToDynamicLoading() {
+        return Allure.step("Navigate to Dynamic Loading page",
+                () -> navigateTo(HomeLinks.DYNAMIC_LOADING, DynamicLoadingPage.class)
+        );
     }
 
-    public HorizontalSliderPage navigateToHorizontalSlider(){
-        return navigateTo(HomeLinks.HORIZONTAL_SLIDER, HorizontalSliderPage.class);
+    public HorizontalSliderPage navigateToHorizontalSlider() {
+        return Allure.step("Navigate to Horizontal Slider page",
+                () -> navigateTo(HomeLinks.HORIZONTAL_SLIDER, HorizontalSliderPage.class)
+        );
     }
 
-    public HoversPage navigateToHovers(){
-        return navigateTo(HomeLinks.HOVERS, HoversPage.class);
+    public HoversPage navigateToHovers() {
+        return Allure.step("Navigate to Hovers page",
+                () -> navigateTo(HomeLinks.HOVERS, HoversPage.class)
+        );
     }
 
-    public JavaScriptAlertsPage navigateToJavaScriptAlerts(){
-        return navigateTo(HomeLinks.JAVASCRIPT_ALERTS, JavaScriptAlertsPage.class);
+    public JavaScriptAlertsPage navigateToJavaScriptAlerts() {
+        return Allure.step("Navigate to JavaScript Alerts page",
+                () -> navigateTo(HomeLinks.JAVASCRIPT_ALERTS, JavaScriptAlertsPage.class)
+        );
     }
 
-    public KeyPressesPage navigateToKeyPresses(){
-        return navigateTo(HomeLinks.KEY_PRESSES, KeyPressesPage.class);
+    public KeyPressesPage navigateToKeyPresses() {
+        return Allure.step("Navigate to Key Presses page",
+                () -> navigateTo(HomeLinks.KEY_PRESSES, KeyPressesPage.class)
+        );
     }
 
-    public LoginPage navigateToFormAuthentication(){
-        return navigateTo(HomeLinks.FORM_AUTHENTICATION, LoginPage.class);
+    public LoginPage navigateToFormAuthentication() {
+        return Allure.step("Navigate to Form Authentication page",
+                () -> navigateTo(HomeLinks.FORM_AUTHENTICATION, LoginPage.class)
+        );
     }
 
-    public WYSIWYGEditorPage navigateToWYSIWYGEditor(){
-        return navigateTo(HomeLinks.WYSIWYG_EDITOR, WYSIWYGEditorPage.class);
+    public WYSIWYGEditorPage navigateToWYSIWYGEditor() {
+        return Allure.step("Navigate to WYSIWYG Editor page",
+                () -> navigateTo(HomeLinks.WYSIWYG_EDITOR, WYSIWYGEditorPage.class)
+        );
     }
 }

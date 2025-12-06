f3e174a1f42f8112a591695897f2a1c9322a1f3a
emre-banking <emrealagoz@gmail.com>
2025-12-07 01:45:53 +0300

    Add Allure.step for FrameTests

--- src/test/java/e2e/frames/FrameTests.java
+++ src/test/java/e2e/frames/FrameTests.java
@@ -6,22 +6,20 @@ import e2e.base.BaseTests;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEquals;
-
 @Listeners({io.qameta.allure.testng.AllureTestNg.class})
 public class FrameTests extends BaseTests {
 
     @Test
-    public void testTextEditor(){
-        // Click on the WYSIWYG Editor link in the home page and navigate to the WYSIWYG Editor page
+    public void verifyTextEditor(){
+        // Given
         var editorPage = homePage.navigateToWYSIWYGEditor();
 
-        // Clear the text area in the editor, set new text, and click the "Decrease Indent" button
+        // When
         editorPage.clearTextArea();
         editorPage.setTextArea("TAU rocks!");
         editorPage.clickDecreaseIndent();
 
-        // Get the text from the editor and verify that it matches the expected text
-        assertEquals(editorPage.getTextFromEditor(), "TAU rocks!");
+        // Then
+        editorPage.assertEditorText("TAU rocks!");
     }
 }
